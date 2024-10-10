4d0363d29a259b27ec1435c96f5137bf1241fad4
coden0w <alexandru.robert@babelgroup.com>
2024-10-10 20:56:50 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import fulldev from 'fulldev-ui/integration';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        fulldev({
+            colors: {
+                theme: 'dark',
+                dark: {
+                    background: '#1A1A1A',
+                    base: '#FA7D23',
+                    brand: '#8423FA'
+                }
+            }
+        })
+    ]
+});
